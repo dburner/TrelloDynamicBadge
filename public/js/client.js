abb7aa14ce307e524396d4c13aa2c3807ca206dc
Glitch (trello-power-up-skeleton) <none>
2023-04-02 19:32:31 +0000

    🛎🚏 Checkpoint ./public/js/client.js:56620044/2431

--- public/js/client.js
+++ public/js/client.js
@@ -5,8 +5,7 @@ var Promise = TrelloPowerUp.Promise;
 var BLACK_ROCKET_ICON = "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421";
 var PRICE_TAG_ICON = "https://cdn.glitch.global/6c3fd098-187e-4506-a7cc-c21dbb301f70/price-tag-icon.png?v=1680461173244";
 
-window.TrelloPowerUp.initialize({
-  "card-badges": function (t, opts) {
+var badgeFunction = function (t, opts) {
     let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
     return t
       .card("name", "desc")
@@ -26,10 +25,10 @@ window.TrelloPowerUp.initialize({
               const regex = /- \*\*Price\*\*: (.{1,50})\n/;
               const match = cardDesc.match(regex);
 
-              const ids = match.length == 2 ? match[1] : " no match";
+              const price = match.length == 2 ? match[1] : " no match";
 
               return {
-                text: "Price " + ids.toString(),
+                text: "Price " + price.toString(),
                 icon: PRICE_TAG_ICON, //"./images/icon.svg",
                 color: "light-gray",
                 refresh: 10, // in seconds
@@ -38,7 +37,12 @@ window.TrelloPowerUp.initialize({
           },
         ];
       });
-  },
+}
+
+window.TrelloPowerUp.initialize({
+  "card-detail-badges": badgeFunction,
+  
+  "card-badges": badgeFunction,
 
   "list-sorters": function (t) {
     return t.list("name", "id").then(function (list) {
